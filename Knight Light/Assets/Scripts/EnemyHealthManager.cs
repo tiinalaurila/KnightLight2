5a0aa85c5121aea45d2613cc69b6aa63f764b838
tiinalaurila <35973720+tiinalaurila@users.noreply.github.com>
2018-02-28 02:06:58 +0200

    Update3

---
+++ Knight Light/Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour {
+
+    public int enemyHealth;
+    public GameObject deathEffect;
+    public int pointsOnDeath;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
